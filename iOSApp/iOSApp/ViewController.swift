17fa172652861f4b1805bc63a4a8892c908ab3a7
Peter Hitchcock <phitchcock@me.com>
2016-03-02 01:01:44 -0800

    installed cocoapods, success response from heroku

--- iOSApp/iOSApp/ViewController.swift
+++ iOSApp/iOSApp/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request(.GET, "https://sac-immunize.herokuapp.com/api/v1/locations")
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
     }
 
     override func didReceiveMemoryWarning() {
