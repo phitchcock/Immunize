a80f5c2a743deb01e2d3da0d10aefcf1ec7e9835
phitchcock <phitchcock@me.com>
2016-03-06 15:29:36 -0800

    testing admin notification controller

--- RailsApp/app/controllers/admin/notifications_controller.rb
+++ RailsApp/app/controllers/admin/notifications_controller.rb
@@ -7,45 +7,17 @@ module Admin
     #   super
     #   @resources = Notification.all.paginate(10, params[:page])
     # end
-    def index
-      @notifications = Rpush::Apns::Notification.all
-    end
 
     def create
-    # Create app first
-    # app = Rpush::Apns::App.new
-    # app.name = "iOSApp"
-    # app.certificate = File.read("lib/certs/sandbox.pem")
-    # app.environment = "sandbox" # APNs environment.
-    # app.password = ""
-    # app.connections = 1
-    # app.save!
-    @notification = Notification.new(notification_params)
+        @notification = Notification.new(notification_params)
 
-    if @notification.save
-      send_notification(@notification)
-      redirect_to notifications_path
-    else
-      render :new
+        if @notification.save
+          NotificationController.send_notification(@notification)
+          redirect_to notifications_path
+        else
+          render :new
+        end
     end
-  end
-
-  private
-
-  def notification_params
-    params.require(:notification).permit!
-  end
-
-  def send_notification(notification)
-    Device.all.each do |d|
-      n = Rpush::Apns::Notification.new
-      n.app = Rpush::Apns::App.find_by_name("iOSApp")
-      n.device_token = d.device_token #64-character hex string
-      n.alert = notification.message
-      n.data = { data: notification.title }
-      n.save!
-    end
-  end
 
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
