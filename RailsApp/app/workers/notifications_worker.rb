6a08ea43aaadef4bcc1255fb564180b2fdd2f4df
phitchcock <phitchcock@me.com>
2016-03-07 11:07:55 -0800

    still testing sidekiq settings

--- RailsApp/app/workers/notifications_worker.rb
+++ RailsApp/app/workers/notifications_worker.rb
@@ -1,11 +1,18 @@
 class NotificationsWorker
   include Sidekiq::Worker
-  include NotificationsHelper
+  #include NotificationsHelper
   
   #sidekiq_options queue: "high"
   # sidekiq_options retry: false
   
   def perform(notification)
-    send_notification(notification)
+    Device.all.each do |d|
+      n = Rpush::Apns::Notification.new
+      n.app = Rpush::Apns::App.find_by_name("iOSApp")
+      n.device_token = d.device_token #64-character hex string
+      n.alert = notification.message
+      n.data = { data: notification.title }
+      n.save!
+    end
   end
 end
