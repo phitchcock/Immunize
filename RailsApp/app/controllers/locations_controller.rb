8ba7192a9b7f1c9f740ec93e0d91298d14050516
Peter Hitchcock <phitchcock@me.com>
2016-03-09 17:14:47 -0800

    starting marker popout style

--- RailsApp/app/controllers/locations_controller.rb
+++ RailsApp/app/controllers/locations_controller.rb
@@ -31,6 +31,8 @@ class LocationsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
       marker.lat location.latitude
       marker.lng location.longitude
+      marker.infowindow location.name
+      marker.json({title: location.name})
     end
   end
 
