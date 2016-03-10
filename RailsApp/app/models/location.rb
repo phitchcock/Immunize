eb4615cb84691e0220bfe4775b5442097e136b16
Peter Hitchcock <phitchcock@me.com>
2016-03-09 16:02:41 -0800

    installed geocoder and gmaps4rails, geocoding address working

--- RailsApp/app/models/location.rb
+++ RailsApp/app/models/location.rb
@@ -17,5 +17,12 @@
 #
 
 class Location < ActiveRecord::Base
-   validates :name, :street_number, :street_name, :city, :state, :zip, :date, :time, :info, presence: true 
+   validates :name, :street_number, :street_name, :city, :state, :zip, :date, :time, :info, presence: true
+
+   geocoded_by :address
+   after_validation :geocode
+
+   def address
+    ["#{street_number} #{street_name}", city, state, zip].compact.join(', ')
+   end
 end
--- RailsApp/db/schema.rb
+++ RailsApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308215929) do
+ActiveRecord::Schema.define(version: 20160309235125) do
 
   create_table "devices", force: :cascade do |t|
     t.string   "device_token", limit: 64
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20160308215929) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.text     "info"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "notifications", force: :cascade do |t|
